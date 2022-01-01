dab4a62a185851b958eb5e33f3f1b56857dc261c
Andy <90814469+paraphia@users.noreply.github.com>
2022-01-01 10:01:26 +0500

    Update example2.py

--- example2.py
+++ example2.py
@@ -5,10 +5,9 @@ def is_even(number):
     return number % 2
 
 class TestEven(unittest.TestCase):
-    # начинается с test_
     def test_even(self):
         test_inputs = [2,3,4,8,15,16,29,54,20,15,75,34,26] # Данные для тестирования
-        test_output = [0,1,0,0,1,0,1,0,0,1,1,0,1] # Ожидаемый ответ
+        test_output = [0,1,0,0,1,0,1,0,0,1,1,0,1] # Ожидаемые ответы
         for i in range(len(test_inputs)):
             self.assertEqual(is_even(test_inputs[i]),test_output[i])
 
